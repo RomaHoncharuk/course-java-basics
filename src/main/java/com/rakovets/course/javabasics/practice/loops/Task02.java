3ec21f9245bfd2d561de1f8835e09b565d3034af
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-07 18:28:19 +0300

    Task01 Done

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
@@ -25,13 +25,16 @@ public class Task02 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        if (headerRowEnable != false) {
-            for (int i = 2; i < rows; i++)
-                System.out.println(i);
+        if (headerRowEnable == true) {
+            for (int i = 1; i <= rows; i++)
+                System.out.print(i +"\n");
+            }else if  {
+            for (int a = 1; a <= rows; a++)
+                System.out.print(a + "\n");
             }else {
-                for (int i = 1; i < rows; i++)
-                    System.out.println(i);
+                System.out.print(1);
             }
+
 
     }
 }
