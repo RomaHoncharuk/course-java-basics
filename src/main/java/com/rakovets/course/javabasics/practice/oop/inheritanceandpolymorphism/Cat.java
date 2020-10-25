d5befa439a636938b30a5da4b00bb369ceccd376
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-25 11:05:32 +0300

    1234

--- src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/Cat.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/Cat.java
@@ -4,22 +4,27 @@ public class Cat {
     public String name;
 
     public Cat(String name) {
+
         this.name = name;
     }
 
     public String mew() {
-       return "mew";
+
+        return "mew";
     }
 
     public String purr() {
+
         return "myrr";
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 }
--- src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/Person.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/Person.java
@@ -3,8 +3,19 @@ package com.rakovets.course.javabasics.practice.oop.inheritanceandpolymorphism;
 public class Person {
     public int happiness;
 
-    public Person(int percentHappi) {
-        this.happiness = percentHappi;
+    public Person(int happiness) {
+        this.happiness = happiness;
     }
 
+    public int changeHappiness(int percentHappiness) {
+        return happiness += percentHappiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
 }
