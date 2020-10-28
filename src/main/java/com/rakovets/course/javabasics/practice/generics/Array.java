c449c52597223ac7b9e569dc3e2e52c616723d5b
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-28 14:56:02 +0300

    1

--- src/main/java/com/rakovets/course/javabasics/practice/generics/Array.java
+++ src/main/java/com/rakovets/course/javabasics/practice/generics/Array.java
@@ -64,15 +64,15 @@ public class Array <T> {
         return s;
     }
 
-    public void maxArray () {
-        int minZnach = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (minZnach < array[i]) {
-                minZnach = array[i];
-            }
-        }
-
-        return minZnach;
-
-    }
+//    public void maxArray () {
+//        int minZnach = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (minZnach < array[i]) {
+//                minZnach = array[i];
+//            }
+//        }
+//
+//        return minZnach;
+//
+//    }
 }
---
+++ src/main/java/com/rakovets/course/javabasics/practice/generics/Pair.java
@@ -0,0 +1,5 @@
+package com.rakovets.course.javabasics.practice.generics;
+
+public class Pair {
+
+}
