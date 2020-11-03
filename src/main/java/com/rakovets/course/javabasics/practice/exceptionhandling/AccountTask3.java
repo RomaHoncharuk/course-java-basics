ebba6091340f1e9e3aebb06ff78a441693751b5e
RomaHoncharuk <orgtexby1@gmail.com>
2020-11-03 20:06:29 +0300

    1.2.3

--- src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/AccountTask3.java
+++ src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/AccountTask3.java
@@ -1,10 +1,38 @@
 package com.rakovets.course.javabasics.practice.exceptionhandling;
 
+import com.rakovets.course.javabasics.practice.exceptionhandling.exeption.AccountExeptions;
+
 public class AccountTask3 {
-    private int demage;
+    private int damage;
+    private boolean stateHero;
 
-    public void hitponsHerro (int demage) {
-        this.demage = demage;
-
+    public AccountTask3 (int damage, boolean stateHero) {
+        this.damage = damage;
+        this.stateHero = stateHero;
     }
+
+    public void hitponsHero (int damage) throws AccountExeptions {
+        if (stateHero) {
+            throw new AccountExeptions();
+        }
+        this.damage -= damage;
+        System.out.print("You Hero whit damage");
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+
+    public boolean getStateHero() {
+        return stateHero;
+    }
+
+    public void setStateHero(boolean stateHero) {
+        this.stateHero = stateHero;
+    }
+
 }
--- src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/Task3.java
+++ src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/Task3.java
@@ -1,6 +1,15 @@
 package com.rakovets.course.javabasics.practice.exceptionhandling;
 
+import com.rakovets.course.javabasics.practice.exceptionhandling.exeption.AccountExeptions;
+
 public class Task3 {
     public static void main(String[] args) {
+        AccountTask3 accountTask3;
+        accountTask3 = new AccountTask3(100, true);
+        try {
+        accountTask3.hitponsHero(50);
+        } catch (AccountExeptions e){
+            System.out.print("Your Hero is dead");
+        }
     }
 }
